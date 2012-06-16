d695e12d4bd90f8e01b1ebbccd30c3f4798a1f97
Developer <developer@Jeffrey-Bairds-MacBook-Pro.local>
2012-06-16 11:12:44 -0400

    initialized the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kngroo::Application.config.secret_token = '874d77efb1ec7b2319dffa3582bf5d54124b72892d455274655ca23d31ab9ebddb189bf7e5f517ea35a32799ebd682e125559fe28533cda8ccfbe564c4cbcc10'
